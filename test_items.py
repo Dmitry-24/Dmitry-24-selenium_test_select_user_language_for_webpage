3b4fc504783373737ddb0a0ae4819b6ec6fd425e
Дмитрий Перфильев <Dmitry-perfilev94@yandex.ru>
2022-11-09 13:36:20 +0300

    modification test

--- test_items.py
+++ test_items.py
@@ -9,5 +9,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_select_user_language_for_webpage(browser):
     browser.get(link)
     browser.implicitly_wait(10)
-    basket_batton = browser.find_elements(By.CSS_SELECTOR, ".add-to-basket>[type='submit']")
-    assert basket_batton, 'basket_batton not found'
+    basket_button = browser.find_elements(By.CSS_SELECTOR, ".add-to-basket>[type='submit']")
+    assert basket_button, 'basket_button not found'
